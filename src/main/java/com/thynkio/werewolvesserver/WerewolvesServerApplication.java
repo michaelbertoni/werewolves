6dc3ecfe2d2ea8a554366b871e34224af28493c5
Michael Bertoni <michael@thynkio.com>
2021-01-26 16:06:44 -0500

    repo initialization

---
+++ src/main/java/com/thynkio/werewolvesserver/WerewolvesServerApplication.java
@@ -0,0 +1,13 @@
+package com.thynkio.werewolvesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerewolvesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WerewolvesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thynkio/werewolvesserver/WerewolvesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thynkio.werewolvesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerewolvesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
